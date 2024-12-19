7701d344be4959b321e2a240f2f6e6aebc7c9e17
Yash Nagla <naglayash@gmail.com>
2024-12-19 14:12:47 +0530

    Created

---
+++ Python/Variable.py
@@ -0,0 +1,83 @@
+# x = 5
+# print(x)
+
+# y = "Yash"
+# print(y)
+
+# print(type(x))
+
+# # Type Casting
+# x = str(5)
+# print(x)
+# print(type(x))
+# x = float(5)
+# print(x)
+# print(type(x))
+
+# a = "Nipun"
+# print(a)
+# b = 'Harsh'
+# print(b)
+
+# print("Camel Case : helloWorld")
+# print("Pascal Case : HelloWorld")
+# print("Snake Case : hello_world")
+
+# print("Assigning Multple values to multiple variables :-")
+# x, y, z = "Red","Blue","Green"
+# print(x)
+# print(y)
+# print(z)
+
+# print("Assigning Single values to multiple variables :-")
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# print("Global Variable :-")
+# gv = "Global Variable"
+
+# def myfunc():
+#     print("Python is " + gv)
+# myfunc()
+
+# # Random Number
+# import random
+# print(random.randint(1, 10))
+
+
+# Data Types
+
+# str = "Hello World"
+# print(str[1])
+# print(len(str[3]))
+# print("Hello" in str)
+# print("Hello" not in str)
+
+# print(str[2:])
+# print(str[2:len(str)-1:2])
+
+# F-Strings
+# age = 36
+# txt = f"My name is John, I am {age}"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price * 2} dollars"
+# print(txt)
+
+# print(bool("Hello"))
+# print(bool(12))
+# print(bool(0))
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+print(mylist)
+
+mylist[1] = "Potato"
+print(mylist)
+
+mylist.insert(len(mylist), "Banana")
+print(mylist)
+
