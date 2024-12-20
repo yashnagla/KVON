348953d4606fb4cd5116da900acd055c4f1e7474
Yash Nagla <naglayash@gmail.com>
2024-12-20 15:43:29 +0530

    Created and Updated

---
+++ Python/practice-set2.py
@@ -0,0 +1,8 @@
+# Q1 WAP to input user's first name & print its length.
+fName = input("Enter Your First Name :- ")
+print("Your Name is ", fName, " and its length is ", int(len(fName)))
+
+
+# Q2 WAP to find the occurrence of '$' in a String.
+a = "Hi, $ i'n the $ symbol $99.99"
+print("Occurrence of '$' in a String is ", a.count("$"), " times")
