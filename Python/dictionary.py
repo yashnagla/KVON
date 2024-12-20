a9dc9c1b1ecd79e5f0d53db460829508420cebff
Yash Nagla <naglayash@gmail.com>
2024-12-20 19:07:45 +0530

    Created and Updated

---
+++ Python/dictionary.py
@@ -0,0 +1,31 @@
+student = {
+    "fName" : "Yash",
+    "lName" : "Nagla",
+    "age" : 23,
+    "domain" : "WebDevelopment",
+    "knownLanguages" : ["C", "C++", "Java", "JavaScript", "Python"],
+    "cgpa" : 7.5,
+    "isFromPoornima" : True,
+    "friends" : ("Vikas", "Nillanshu", "Nipun", "Harsh", "Ashutosh"),
+    "semScore" : {
+        "Sem1" : 7.93,
+        "Sem2" : 8.73,
+        "Sem3" : 6.47,
+        "Sem4" : 6.31,
+        "Sem5" : 6.67,
+        "Sem6" : 6.87,
+        "Sem7" : 8.43,
+        "Sem8" : 8.45
+    }
+}
+
+print(student)
+print(student["fName"])
+print(student)
+print(student["lName"])
+print(student["age"])
+print(student["domain"])
+print(student["knownLanguages"])
+print(student["cgpa"])
+print(student["friends"])
+print(student["semScore"])
