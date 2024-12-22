201f98c26ee6ffb85d02623ba611a764f08f0b3b
Yash Nagla <naglayash@gmail.com>
2024-12-22 17:24:33 +0530

    Updated

--- Python/practice-set11-recursive-functions.py
+++ Python/practice-set11-recursive-functions.py
@@ -1,9 +1,9 @@
 # Q1 Write a recursive function to calculate the sum of first n natural numbers.
-# def sum(n) :
-#     if (n == 0):
-#         return 0
-#     return sum(n - 1) + n
-# print(sum(5))
+def sum(n) :
+    if (n == 0):
+        return 0
+    return sum(n - 1) + n
+print(sum(5))
 
 # Q2 Write a recursive function to print all elements in a list. Hint : use list & index as parameters.
 list1 = ["Mango", "Kiwi", "Apple", "Banana"]
