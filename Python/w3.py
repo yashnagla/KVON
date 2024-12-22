3c6c1b00ea12ecaa69f1c7ca2cef0ef6e62fd0d9
Yash Nagla <naglayash@gmail.com>
2024-12-22 22:51:52 +0530

    Created and Updated

---
+++ Python/w3.py
@@ -0,0 +1,24 @@
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+# (green, yellow, *red) = fruits
+# print(green)
+# print(yellow)
+# print(red)
+
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#     print(x)
+ 
+# thistuple = ("apple", "banana", "cherry")
+# for i in range(len(thistuple)):
+#     print(thistuple[i])
+
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2
+# print(mytuple)
+
+tuple1 = ('a', 'b' , 'c')
+tuple2 = (1, 2, 3)
+tuple3 = tuple2 + tuple1
+print(tuple3)
