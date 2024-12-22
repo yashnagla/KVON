fc0b657d21116407b212b34eec02f4b0cef2a852
Yash Nagla <naglayash@gmail.com>
2024-12-22 22:23:42 +0530

    Created and Updated

--- Python/file-io.py
+++ Python/file-io.py
@@ -1,34 +1,34 @@
 # Reading File
 
-# f = open("demo.txt", "r")
-# data = f.read()
-# line1 = f.readline()
-# line2 = f.readline()
-# print(data)
-# print(line1)
-# print(line2)
-# print(type(data))
-# f.close()
+f = open("demo.txt", "r")
+data = f.read()
+line1 = f.readline()
+line2 = f.readline()
+print(data)
+print(line1)
+print(line2)
+print(type(data))
+f.close()
 
 # Writing File
 
-# w = open("demo.txt", "w")
-# w.write("I am using w to override the file.")
-# w.close()
+w = open("demo.txt", "w")
+w.write("I am using w to override the file.")
+w.close()
 
 # Append in File
 
-# a = open("demo.txt", "a")
-# a.write("I an using a to append the text in the end of the file.")
-# a.close()
+a = open("demo.txt", "a")
+a.write("I an using a to append the text in the end of the file.")
+a.close()
 
-# with open("demo.txt", "r") as f :
-#     data = f.read()
-#     print(data)
+with open("demo.txt", "r") as f :
+    data = f.read()
+    print(data)
 
-# with open("demo.txt", "w") as f :
-#     f.write("New Data")
-#     print(data)
+with open("demo.txt", "w") as f :
+    f.write("New Data")
+    print(data)
 
-# import os
-# os.remove("demo.txt")
+import os
+os.remove("demo.txt")
