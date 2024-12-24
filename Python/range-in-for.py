990cc7508eca825503c79a114ddd95625476755b
Yash Nagla <naglayash@gmail.com>
2024-12-24 20:48:24 +0530

    Updated

--- Python/range-in-for.py
+++ Python/range-in-for.py
@@ -1,18 +1,18 @@
-# list = [1,23,4,5,6,67]
-# for i in range(len(list)):
-#     print(list[i])
+list = [1,23,4,5,6,67]
+for i in range(len(list)):
+    print(list[i])
 
-# x = int(input("Enter"))
-# if (x % 2 == 0):
-#     print("Even")
-# else:
-#     print("Odd")
+x = int(input("Enter"))
+if (x % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
 
-# print(8)
-# print(13, end=" ")
-# print(21)
+print(8)
+print(13, end=" ")
+print(21)
 
-# print(7, "\n", 5)
+print(7, "\n", 5)
 
 a = input()
 b = input()
